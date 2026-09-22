8c86ee5535ebe13b7a369902efb79e31b0a0fff8
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2026-09-23 01:14:52 +0900

    refactor: MySQL Testcontainers 정의를 MySQLTestContainerConfig로 이동
    - DB 컨테이너 정의는 MySQLTestCOntainerConfig에서 설정
    - @DynamicPropertySource는 Spring 제약상 테스트 클래스 자신/상위 클래스에 있어야 하므로
      AbstractIntegrationTest에 남기고, 컨테이너 값은 MySQLTestCOntainerConfig를 참조하도록 분리

--- src/test/java/com/board/testSupport/AbstractIntegrationTest.java
+++ src/test/java/com/board/testSupport/AbstractIntegrationTest.java
@@ -2,33 +2,25 @@ package com.board.testSupport;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
 
 @ActiveProfiles("test")
 @Tag("integrationTest")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Import(MySQLTestContainerConfig.class)
 public class AbstractIntegrationTest {
-
-    static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:9.7")
-            .withDatabaseName("board")
-            .withUsername("application")
-            .withPassword("application");
-
-    static {
-        MYSQL_CONTAINER.start();
-    }
 
     @DynamicPropertySource
     static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.url", MySQLTestContainerConfig.MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MySQLTestContainerConfig.MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MySQLTestContainerConfig.MYSQL_CONTAINER::getPassword);
     }
 }
--- src/test/java/com/board/testSupport/MySQLTestContainerConfig.java
+++ src/test/java/com/board/testSupport/MySQLTestContainerConfig.java
@@ -1,8 +1,17 @@
 package com.board.testSupport;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.testcontainers.containers.MySQLContainer;
 
 @TestConfiguration
 public class MySQLTestContainerConfig {
 
+    static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:9.7")
+            .withDatabaseName("board")
+            .withUsername("application")
+            .withPassword("application");
+
+    static {
+        MYSQL_CONTAINER.start();
+    }
 }
